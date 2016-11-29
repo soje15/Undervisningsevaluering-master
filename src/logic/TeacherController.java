aea2e5c77ca207b615e8a7c9bedc74e594e140e8
Søren Kolbye Jensen <soje15ac@student.cbs.dk>
2016-11-29 08:12:16 +0100

    Uploaded code made in cooperation with the rest of the Undervisnings-evaluerings team, which was never pushed to Github.

--- src/logic/TeacherController.java
+++ src/logic/TeacherController.java
@@ -15,6 +15,9 @@ import java.util.Map;
  * Controllerklasse der håndterer metoder/funktioner specifikker for Underviser-brugeren.
  * Arver fra UserController.
  */
+
+
+//Forgot comment in last commit ;)
 public class TeacherController extends UserController {
 
     public static void main(String[] args) {
